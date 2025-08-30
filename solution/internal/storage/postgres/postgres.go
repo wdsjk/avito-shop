7340e86b12548803c59a7fbbab73b0478fe79fda
wdsjk <mamaevivan543@gmail.com>
2025-08-30 14:22:13 +0300

    add docker compose with db init, change db prepare statements

--- solution/internal/storage/postgres/postgres.go
+++ solution/internal/storage/postgres/postgres.go
@@ -27,27 +27,69 @@ func New(
 	}
 
 	stmt, err := db.Prepare(`
-	CREATE TABLE IF NOT EXISTS employees (
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(50) NOT NULL UNIQUE,
-		coins INT CHECK (coins > -1),
-		bought_items VARCHAR(50)[] REFERENCES items(name)
-	);
 	CREATE TABLE IF NOT EXISTS items (
 		id SERIAL PRIMARY KEY,
-		name VARCHAR(50) NOT NULL,
-		price INT CHECK (price > 0) NOT NULL
-	);
-	CREATE TABLE IF NOT EXISTS transfers (
-		id SERIAL PRIMARY KEY,
-		sender_id INT REFERENCES employees(id),
-		receiver_id INT REFERENCES employees(id),
-		amount INT CHECK (amount > 0) NOT NULL
+		name VARCHAR(50) NOT NULL UNIQUE,
+		price NUMERIC CHECK (price > 0) NOT NULL
 	);`)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
 
+	stmt, err = db.Prepare(`
+	INSERT INTO items (name, price)
+	SELECT name, price
+	FROM (VALUES
+		('t-shirt', 80),
+		('cup', 20),
+		('book', 50),
+		('pen', 10),
+		('powerbank', 200),
+		('hoody', 300),
+		('umbrella', 200),
+		('socks', 10),
+		('wallet', 50),
+		('pink-hoody', 500)
+	) AS new_items(name, price)
+	WHERE NOT EXISTS (SELECT 1 FROM items LIMIT 1);
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	stmt, err = db.Prepare(`
+	CREATE TABLE IF NOT EXISTS employees (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(50) NOT NULL UNIQUE,
+		coins NUMERIC CHECK (coins > -1),
+		bought_items VARCHAR(50)[]
+	);`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	stmt, err = db.Prepare(`
+	CREATE TABLE IF NOT EXISTS transfers (
+		id SERIAL PRIMARY KEY,
+		sender_id INT REFERENCES employees(id),
+		receiver_id INT REFERENCES employees(id),
+		amount NUMERIC CHECK (amount > 0) NOT NULL
+	);`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
 	_, err = stmt.Exec()
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
